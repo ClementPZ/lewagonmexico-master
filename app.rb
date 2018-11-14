368c3ad0a0bd662d6bb1354f2eb4eadb6202ac3d
Clement PZ <clementpicardzivy@gmail.com>
2018-11-14 09:14:15 -0600

    Rodrigo 30k

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ get '/' do
 end
 
 post '/charge' do
-  @amount = 314781
+  @amount = 556250
 
   customer = Stripe::Customer.create(
     :email => params[:email],
@@ -57,7 +57,7 @@ __END__
       data-key="<%= settings.publishable_key %>"
       data-name="Fast Foo Treinamentos"
       data-image="https://le-wagon-mexico-pagos.herokuapp.com/images/logo_circle.png"
-      data-amount="314781"
+      data-amount="556250"
       data-currency="brl"
       data-description="Le Wagon Bootcamp"
       data-zip-code="true"
