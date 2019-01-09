b1307ae9736c1e9dd9d754f70588d5da838580f3
Clement PZ <clementpicardzivy@gmail.com>
2019-01-09 15:31:50 -0600

    64267

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ get '/' do
 end
 
 post '/charge' do
-  @amount = 975300
+  @amount = 12321
 
   customer = Stripe::Customer.create(
     :email => params[:email],
@@ -57,7 +57,7 @@ __END__
       data-key="<%= settings.publishable_key %>"
       data-name="Fast Foo Treinamentos"
       data-image="https://le-wagon-mexico-pagos.herokuapp.com/images/logo_circle.png"
-      data-amount="975300"
+      data-amount="12321"
       data-currency="brl"
       data-description="Le Wagon Bootcamp"
       data-zip-code="true"
@@ -70,5 +70,5 @@ __END__
 
 @@charge
 <div style="text-align: center; padding-top: 200px;">
-  <h2 style="font-family: helvetica;">Thank you very much for the payment of the first deposit of 50,000 MXN and .. Welcome on board!</h2>
+  <h2 style="font-family: helvetica;">Thank you very much for the payment of the first deposit of 64,267 MXN and .. Welcome on board!</h2>
 </div>
