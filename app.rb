cb76649121f58ab2c808de9799888df047e698e7
Clement PZ <clementpicardzivy@gmail.com>
2019-01-28 12:14:00 -0600

    14267

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ get '/' do
 end
 
 post '/charge' do
-  @amount = 889900
+  @amount = 282400
 
   customer = Stripe::Customer.create(
     :email => params[:email],
@@ -57,7 +57,7 @@ __END__
       data-key="<%= settings.publishable_key %>"
       data-name="Fast Foo Treinamentos"
       data-image="https://le-wagon-mexico-pagos.herokuapp.com/images/logo_circle.png"
-      data-amount="889900"
+      data-amount="282400"
       data-currency="brl"
       data-description="Le Wagon Bootcamp"
       data-zip-code="true"
@@ -70,5 +70,5 @@ __END__
 
 @@charge
 <div style="text-align: center; padding-top: 200px;">
-  <h2 style="font-family: helvetica;">Thank you very much for the payment of 8899 BRL / 45,200 MXN</h2>
+  <h2 style="font-family: helvetica;">Thank you very much for the payment of 14 267 MXN</h2>
 </div>
